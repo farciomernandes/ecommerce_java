e1d4f04b116658f2f568a8ea7353627b0d172598
Marcio Fernandes <farciomernandes@gmail.com>
2025-02-10 14:17:37 -0300

    first commit

---
+++ src/main/java/ecommerce/free/EcommerceFreeApplication.java
@@ -0,0 +1,13 @@
+package ecommerce.free;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecommerce/free/EcommerceFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.free;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
