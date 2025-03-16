90b55479f48618c0f8678ec9ef7b6d37639dce2b
Marcio Fernandes <farciomernandes@gmail.com>
2025-03-16 20:06:00 -0300

    feat: triggers

--- src/main/java/ecommerce/free/infra/db/migrations/V1_16032025.sql
+++ src/main/java/ecommerce/free/infra/db/migrations/V1_16032025.sql
@@ -1,4 +1,4 @@
-/** Cria trigger que valida pessoa pois o spring não reconheceu, ela é abstract
+/** Cria triggers que valida pessoa
 CREATE OR REPLACE FUNCTION validatePeopleKey()
 RETURNS TRIGGER
 LANGUAGE plpgsql
@@ -20,8 +20,114 @@ BEGIN
 END;
 $$;
 
-CREATE TRIGGER validatePeopleKeyProductReview
+CREATE OR REPLACE FUNCTION validatePeopleKey2()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    existe INTEGER;
+BEGIN
+    existe = (SELECT COUNT(1) FROM individual_person WHERE id = NEW.supplier_id);
+
+    IF (existe <= 0) THEN
+        existe = (SELECT COUNT(1) FROM juridical_person WHERE id = NEW.supplier_id);
+
+        IF (existe <= 0) THEN
+            RAISE EXCEPTION 'Não foi encontrado o ID ou PK da pessoa para realizar a associação';
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+########################################################
+
+CREATE TRIGGER validatePeopleKeyProductReviewInsert
+BEFORE INSERT
+ON product_review
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+CREATE TRIGGER validatePeopleKeyProductReviewUpdate
 BEFORE UPDATE
 ON product_review
 FOR EACH ROW
 EXECUTE FUNCTION validatePeopleKey();
+
+####################################################
+CREATE TRIGGER validatePeopleKeyAccountPaymentInsert
+BEFORE INSERT
+on account_payment
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey2();
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentUpdate
+BEFORE UPDATE
+on account_payment
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey2();
+
+######################################################
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentInsert
+BEFORE INSERT
+on account_receivable
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentUpdate
+BEFORE UPDATE
+on account_receivable
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+
+######################################################
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentInsert
+BEFORE INSERT
+on address
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentUpdate
+BEFORE UPDATE
+on address
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+######################################################
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentInsert
+BEFORE INSERT
+on purchase_invoice
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentUpdate
+BEFORE UPDATE
+on purchase_invoice
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+######################################################
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentInsert
+BEFORE INSERT
+on store_sale_purchase
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+CREATE TRIGGER validatePeopleKeyAccountPaymentUpdate
+BEFORE UPDATE
+on store_sale_purchase
+FOR EACH ROW
+EXECUTE FUNCTION validatePeopleKey();
+
+
+
+
+
+
+
